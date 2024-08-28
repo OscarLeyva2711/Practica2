5ef53b187f6896f383714089865f38eae43b7fbf
Oscar  Leyva Herrera <donlevantin@Donlevanton.local>
2024-08-28 10:06:05 -0700

    Primera Version

---
+++ BancoDePalabras.java
@@ -0,0 +1,25 @@
+
+/**
+ * Write a description of class BancoDePalabras here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class BancoDePalabras
+{
+    // instance variables - replace the example below with your own
+    String palabra = "banana";
+
+    /**
+     * Constructor for objects of class BancoDePalabras
+     */
+    public BancoDePalabras()
+    {
+        
+    }
+    public String dameUnaPalabra(){
+        return palabra;
+    }
+}
+    
+    
